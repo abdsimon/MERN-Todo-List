46697f508f1c38c173bb5c88f801c0c92e1efd7b
Simon <diababdulwahed@gmail.com>
2024-03-26 12:40:53 +0100

    new folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import TodoList from './components/TodoList';
+
 
 function App() {
     return (
         <div className="App">
-            <h1>MERN Todo List</h1>
-            <TodoList />
+           
+            
         </div>
     );
 }
