f52b94ff91a222f20b732d0af63eb76e3f1b1d93
Simon <diababdulwahed@gmail.com>
2024-04-02 16:52:13 +0200

    added changes to backend

--- server/server.js
+++ server/server.js
@@ -12,13 +12,6 @@ app.get('/hello', (req, res) => {
     console.log(`Server is running at http://localhost:${port}`)
 });
 
-app.post('/todos', (req, res) => {
-    const { text, completed } = req.body;
-    const newTodo = new Todo({ text, completed });
-    newTodo.save()
-        .then(todo => res.json(todo))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
 
 // Read all Todos
 app.get('/todos', (req, res) => {
