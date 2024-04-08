685108bc604d71f621cc2d46cc0dbc943afd003a
Simon <diababdulwahed@gmail.com>
2024-04-08 13:41:48 +0200

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,67 @@
-// App.js
+
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [newTodo, setNewTodo] = useState('');
 
   useEffect(() => {
-    async function fetchTodos() {
-      try {
-        const response = await axios.get('/api/todos');
-        setTodos(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+    // Fetch todos from the server when component mounts
     fetchTodos();
   }, []);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('/api/todos');
+      setTodos(response.data);
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+    }
+  };
+
+  const handleAddTodo = async () => {
     try {
       const response = await axios.post('/api/todos', {
-        title,
-        description,
+        title: newTodo
       });
       setTodos([...todos, response.data]);
-      setTitle('');
-      setDescription('');
+      setNewTodo('');
     } catch (error) {
-      console.error(error);
+      console.error('Error adding todo:', error);
+    }
+  };
+
+  const handleDeleteTodo = async (id) => {
+    try {
+      await axios.delete(`/api/todos/${id}`);
+      setTodos(todos.filter(todo => todo._id !== id));
+    } catch (error) {
+      console.error('Error deleting todo:', error);
     }
   };
 
   return (
-   
+    <div className="App">
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Enter a new todo"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            {todo.title}
+            <button onClick={() => handleDeleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
