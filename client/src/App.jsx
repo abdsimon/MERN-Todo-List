41aa5636d24f06d033b98b6043fee031ae871712
Simon <diababdulwahed@gmail.com>
2024-07-15 19:34:35 +0200

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   const handleDeleteTodo = async (id) => {
 
     try {
-      await axios.delete(`http://localhost:5000/api/todos/${id}`); smn
+      await axios.delete(`http://localhost:5000/api/todos/${id}`); sn
       on
 
       
